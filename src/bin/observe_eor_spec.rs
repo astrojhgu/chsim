80485177a32556708b0775dd38e3ebc555551ec0
Gu Junhua <jhgu@nao.cas.cn>
2021-11-07 18:09:29 +0800

    modified:   Cargo.toml 	modified:   src/bin/observe_eor_spec.rs

--- src/bin/observe_eor_spec.rs
+++ src/bin/observe_eor_spec.rs
@@ -1,4 +1,4 @@
-use ndarray::{Array1, Array2, ArrayView1, Axis, s};
+use ndarray::{Array1, Array2, ArrayView1, Axis, s, parallel::prelude::*};
 use num_complex::Complex;
 use num_traits::FloatConst;
 use rspfb::{csp_pfb::CspPfb, cspfb, oscillator::COscillator, ospfb, windowed_fir::coeff};
@@ -10,8 +10,8 @@ use ndarray_npy::NpzWriter;
 
 pub fn main(){
     let nch_coarse = 1024;
-    //let coeff_coarse = coeff::<f64>(nch_coarse / 2, 16, 1.1);//best
-    let coeff_coarse = coeff::<f64>(nch_coarse / 2, 12, 1.3);//bad
+    let coeff_coarse = coeff::<f64>(nch_coarse / 2, 16, 1.1);//best
+    //let coeff_coarse = coeff::<f64>(nch_coarse / 2, 12, 1.3);//bad
 
     let nch_fine = 16;
     let coeff_fine = coeff::<f64>(nch_fine*2, 2, 0.5);
@@ -19,8 +19,8 @@ pub fn main(){
 
     let data_len=1<<22;
     let nch=data_len/2;
-    let mut outfile=ndarray_npy::NpzWriter::new(std::fs::File::create("a.npz").unwrap());
-    let fg=chsim::fg_spec(data_len/2, 50e6, 200e6, &|f|{3.0});
+    
+    let fg=chsim::fg_spec(data_len/2, 50e6, 200e6, &|f|{300.0});
 
 
 
@@ -42,28 +42,43 @@ pub fn main(){
     let freq_raw=Array1::from(linspace(0.0, 200e6, data_len/2).collect::<Vec<_>>());
     let freq_obs=Array1::from(linspace(0.0, 200e6, spec.len()).collect::<Vec<_>>());
     
-
-    outfile.add_array("input_spec", &ArrayView1::from(&total_spec));
-    outfile.add_array("freq_raw", &freq_raw);
-    outfile.add_array("freq_obs", &freq_obs);
     //////////
+    let niters=1000000;
+    let ncpus=num_cpus::get();
+    eprintln!("running with {} cores", ncpus);
     
-    
-    for i in 0..100{
-        println!("{}", i);
-        let total_signal:Vec<_>=chsim::simulate_time_series(&total_spec, &mut rand::thread_rng()).iter().map(|x| Complex::<f64>::from(x)).collect();
-        let mut channelizer_coarse = ospfb::Analyzer::<Complex<f64>, f64>::new(nch_coarse, ArrayView1::from(&coeff_coarse));
-        let channelizer_fine =
-            cspfb::Analyzer::<Complex<f64>, f64>::new(nch_fine * 2, ArrayView1::from(&coeff_fine));
+
+
+    let mut temp_spec=Array2::<f64>::zeros((ncpus, nch_coarse*nch_fine/2));
+    for i in 0..niters/ncpus{
+        println!("{} {}", i, i as f64/(niters as f64/ncpus as f64));
+
+        temp_spec.axis_iter_mut(Axis(0)).into_par_iter().for_each(|mut c|{
+            let total_signal:Vec<_>=chsim::simulate_time_series(&total_spec, &mut rand::thread_rng()).iter().map(|x| Complex::<f64>::from(x)).collect();
+            //eprint!("{:.4} ", total_signal[100].re);
+            let mut channelizer_coarse = ospfb::Analyzer::<Complex<f64>, f64>::new(nch_coarse, ArrayView1::from(&coeff_coarse));
+            let channelizer_fine =
+                cspfb::Analyzer::<Complex<f64>, f64>::new(nch_fine * 2, ArrayView1::from(&coeff_fine));
+            
+            let mut csp = CspPfb::new(&selected_ch, &channelizer_fine);
         
-        let mut csp = CspPfb::new(&selected_ch, &channelizer_fine);
-    
-        let mut station_output = channelizer_coarse.analyze_par(&total_signal);
-        let fine_channels:Array2<_> = csp.analyze_par(station_output.view_mut()).slice(s![8..8192+8, 2..]).to_owned();
-        //let spec1=fine_channels.sum_axis(Axis(1));
-        let spec1=fine_channels.map(|x| x.norm_sqr()).sum_axis(Axis(1));
-        spec+=&spec1;    
+            let mut station_output = channelizer_coarse.analyze_par(&total_signal);
+            let fine_channels:Array2<_> = csp.analyze_par(station_output.view_mut()).slice(s![8..8192+8, 2..]).to_owned();
+            //let spec1=fine_channels.sum_axis(Axis(1));
+            let spec1=fine_channels.map(|x| x.norm_sqr()).sum_axis(Axis(1));
+            c.assign(&spec1);
+        });
+        //eprintln!();
+        spec+=&temp_spec.sum_axis(Axis(0));
+
+        if i%10==0||i==niters/ncpus-1{
+            let mut outfile=ndarray_npy::NpzWriter::new(std::fs::File::create("a.npz").unwrap());
+            outfile.add_array("input_spec", &ArrayView1::from(&total_spec));
+            outfile.add_array("freq_raw", &freq_raw);
+            outfile.add_array("freq_obs", &freq_obs);
+            outfile.add_array("spec", &spec);         
+        }
     }
     
-   outfile.add_array("spec", &spec);
+   
 }
